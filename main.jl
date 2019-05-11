045f1c5419cecf1798fa8dbc5d8aa11ccba92640
George3d6 <george@cerebralab.com>
2019-05-11 09:52:56 +0000

    added protein download code

--- main.jl
+++ main.jl
@@ -2,7 +2,9 @@ using Bio.Structure
 
 function main()
     protein_dataset = ["1EN2"]
-    for protein in protein_dataset;
-        
-    downloadpdb("1EN2")
+    for protein_name in protein_dataset
+        if !isfile("protein_data/$protein_name.pdb")
+            downloadpdb(protein_name)
+        end
+    end
 end
