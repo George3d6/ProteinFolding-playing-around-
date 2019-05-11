9e05bb0f6972705b83e8e360dbd70d2a92fe107f
George3d6 <george@cerebralab.com>
2019-05-11 10:34:11 +0000

    aded code to parse the protein data into something we can learn from

--- main.jl
+++ main.jl
@@ -1,10 +1,30 @@
-using Bio.Structure
+using Bio.Structure: coords, downloadpdb, PDB, read, resname, atoms, resnumber, serial, standardselector, AminoAcidSequence
 
-function main()
-    protein_dataset = ["1EN2"]
+function get_protein_names()
+    ["1EN2"]
+end
+
+function download_proteins()
+    protein_dataset = get_protein_names()
     for protein_name in protein_dataset
         if !isfile("protein_data/$protein_name.pdb")
-            downloadpdb(protein_name)
+            downloadpdb(protein_name, pdb_dir="protein_data")
         end
+    end
+end
+
+function main()
+    download_proteins()
+    for protein_name in get_protein_names()
+        protein = read("protein_data/$protein_name.pdb", PDB)
+        for residue in protein["A"]
+            name = resname(residue)
+            println(name)
+            for atom in atoms(residue)
+                location = coords(atom[2])
+                serial = serial(atom[2])
+            end
+        end
+        seq = AminoAcidSequence(struct["A"], standardselector)
     end
 end
