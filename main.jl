829e4ef964373a70711c7607b250a71ba75a5c1a
George3d6 <george@cerebralab.com>
2019-05-18 11:40:43 +0000

    a

--- main.jl
+++ main.jl
@@ -22,8 +22,12 @@ function main()
     atom_dict["NULL"] = 0
     last_atom_index = 0
 
+    residue_dict = Dict{String,Int32}()
+    residue_dict["NULL"] = 0
+    last_residue_index = 0
+
     download_proteins()
-    output_length = 2601
+    output_length = 2460
     for protein_name in get_protein_names()
         protein = read("protein_data/$protein_name.pdb", PDB)
 
@@ -50,18 +54,35 @@ function main()
             end
         end
         seq = AminoAcidSequence(protein["A"], standardselector)
+        seq = String(seq)
+        index_seq = zeros(Int32,length(seq))
+
+        for index in 1:length(seq)
+            residue_name = string(seq[index])
+            residue_index = get(residue_dict, residue_name, false)
+            if residue_index == false
+                residue_index = last_residue_index
+                last_residue_index += 1
+                residue_dict[residue_name] = residue_index
+            end
+            index_seq[index] = residue_index
+        end
 
 
         model = Chain(
-          Dense(UInt32(output_length/3), UInt32(output_length), σ),
-          LSTM(UInt32(output_length), UInt32(output_length)),
-          Dense(UInt32(output_length), UInt32(output_length)),
+          Dense(UInt32(length(index_seq)), UInt32(output_length/6), σ),
+          LSTM(UInt32(output_length/6), UInt32(output_length/12)),
+          LSTM(UInt32(output_length/12), UInt32(output_length/6)),
+          Dense(UInt32(output_length/6), UInt32(output_length)),
           softmax)
-         '''
+        '''
         model = Chain(
-            Dense(UInt32(output_length/3), UInt32(output_length), σ),
-            Dense(UInt32(output_length), UInt32(output_length)),
-            Dense(UInt32(output_length), UInt32(output_length)),
+            Dense(UInt32(length(index_seq)), UInt32(output_length/1.5), σ),
+            Dense(UInt32(output_length/1.5), UInt32(output_length)),
+            Dense(UInt32(output_length), UInt32(output_length*2)),
+            Dense(UInt32(output_length*2), UInt32(output_length*3)),
+            Dense(UInt32(output_length*3), UInt32(output_length*2)),
+            Dense(UInt32(output_length*2), UInt32(output_length)),
             softmax)
         '''
 
@@ -72,17 +93,16 @@ function main()
 
         accuracy(y, y_test) = mean(isapprox.(y, y_test))
 
-        atoms_arr = Tracker.data(atoms_arr)
+        atoms_arr = Tracker.data(index_seq)
         locations_arr = Tracker.data(locations_arr)
 
-        dataset = [(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr)]
+        dataset = [(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr),(atoms_arr, locations_arr)]
 
         @epochs 30 Flux.train!(loss, params(model), dataset, ADAM())
 
         predictions = model(atoms_arr)
 
-        println(predictions)
-        println(locations_arr)
+        println(index_seq)
 
         acc = accuracy(predictions, locations_arr)
         println(acc)
